4bd5f74e1abbf2bf795a45aa8dcd74bcb0f60c3e
dknathalage <dknathalage@gmail.com>
2025-06-25 22:08:44 +1000

    Enhance CLI functionality by adding command handling for 'gen', 'init', and 'apply' commands in main.go; update action.yml to run generator with 'gen' argument.

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Println("Usage: cli <command>")
+		os.Exit(1)
+	}
+
+	if os.Args[1] == "gen" {
+		log.Println("Generating config code")
+	}
+
+	if os.Args[1] == "init" {
+		log.Println("Initializing config code")
+	}
+
+	if os.Args[1] == "apply" {
+		log.Println("Applying config code")
+	}
+}
